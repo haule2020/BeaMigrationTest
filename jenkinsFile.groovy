6906720817fe7fe54de856de030350fd05f69bc4
haule2020 <61819317+haule2020@users.noreply.github.com>
2023-05-23 16:56:12 +0700

    update Jenkinsfile

--- jenkinsFile.groovy
+++ jenkinsFile.groovy
@@ -6,6 +6,31 @@ pipeline {
         git 'https://github.com/haule2020/BeaMigrationTest.git'
       }
     }
+    stage('Test Execution') {
+      steps {
+        {
+            script {
+                sh (mvn test -DskipFailures=false -Dkarate.options='--tags @apiTest')
+            }
+        }     
+      }
+    }
+
+    stage('Test Execution') {
+      steps {
+        {
+            script {
+                publishHTML (target: [
+                    allowMissing: false, 
+                    alswaysLinkToLastBuild: true, 
+                    keepAll: true,  
+                    reportDir: 'target/cucumber-html-report/',
+                    reportFiles: 'overview-features.html',
+                    reportName: 'KarateDemo Report'])
+            }
+        }     
+      }
+    }
 
   }
 }
