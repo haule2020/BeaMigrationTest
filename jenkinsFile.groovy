8cb80a903fb5c87d9df52fb0c2ff8242663386c1
haule2020 <61819317+haule2020@users.noreply.github.com>
2023-06-05 14:39:12 +0700

    UPDATE

--- jenkinsFile.groovy
+++ jenkinsFile.groovy
@@ -1,38 +1,21 @@
 pipeline {
   agent any
-
   tools {
         maven 'Maven 3.9.2'
     }
   stages {
-    // stage('Checkout Scm') {
-    //   steps {
-    //     git 'https://github.com/haule2020/BeaMigrationTest.git'
-    //   }
-    // }
     stage('Test Execution') {
       steps {
-        
-            script {
-                sh ("mvn test -DskipFailures=false -Dkarate.options='--tags @apiTest'")
-            }
-           
+        script {
+             try {
+                sh ("mvn test -DskipFailures=false -Dkarate.options='--tags @apiTest'")      
+                  }
+              finally
+                {
+                publishHTML (target: [allowMissing: false, keepAll: true,  reportDir: 'target/cucumber-html-reports/',reportFiles: 'overview-features.html',reportName: 'KarateDemo Report'])
+           }    
+        }    
       }
     }
-
-    stage('Upload report') {
-      steps {
-        
-            script {
-                publishHTML (target: [
-                    allowMissing: false, 
-                    keepAll: true,  
-                    reportDir: 'target/cucumber-html-reports/',
-                    reportFiles: 'overview-features.html',
-                    reportName: 'KarateDemo Report'])
-            }           
-      }
-    }
-
   }
 }
